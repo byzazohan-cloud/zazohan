bcc9647c6fb37eee9ed142555a3fd1172daea49c
byzazohan-cloud <byzazohan@gmail.com>
2026-09-20 13:41:31 +0300

    zazoplayers temizle

--- config.js
+++
@@ -1,6 +0,0 @@
-// ZAZO PLAYER uygulama sahibi ayarları.
-// Bu dosyayı yayınlamadan önce kendi kısıtlanmış anahtarlarınla doldurabilirsin.
-window.ZAZO_CONFIG = {
-  youtubeApiKey: '',
-  jamendoClientId: ''
-};
