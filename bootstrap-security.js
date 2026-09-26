c572d76e9840c90af683c8b166f1ca802dcf8ebf
byzazohan-cloud <byzazohan@gmail.com>
2026-09-26 20:57:51 +0300

    Add files via upload

--- bootstrap-security.js
+++ bootstrap-security.js
@@ -1,6 +1,6 @@
 'use strict';
 (() => {
-  const BUILD='20260925-HANE-V0.15.16-FIX10-PREMIUM-LOCK';
+  const BUILD='20260926-HANE-V0.15.16-FIX18-HISTORY-MIGRATION';
   const KEY='hane_app_shell_build';
   const RELOAD_KEY='hane_auto_update_reload';
   const CHECK_MS=60*60*1000;
